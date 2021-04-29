925df92a955d64f1bdd2ab7e1143fb0a4c85fca0
Deb0912 <83256644+Deb0912@users.noreply.github.com>
2021-04-28 20:07:36 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ $(function() {
             tag: "fitness"
         },
         {
-            imageUrl: "images/portfolio-items/Freebie-What-to-Eat-for-That.pdf",
+            imageUrl: "images/portfolio-items/Freebie-What-to-Eat-for-That-1.pdf",
             title: "Vital Change Now Program",
             tag: "fitness"
         },
